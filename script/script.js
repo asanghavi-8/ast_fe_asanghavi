2e7204ecf0f37d7eb38f5581d28b9e4f42ceac16
Ananya Sanghavi <a.sanghavi@astounddigital.com>
2024-12-17 17:16:48 +0530

    Lab 4: Advanced CSS - Animations & Transitions

--- script/script.js
+++ script/script.js
@@ -2,7 +2,6 @@ let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  console.log("Hello");
   let i;
   let slides = document.getElementsByClassName("slide");
   for (i = 0; i < slides.length; i++) {
@@ -15,21 +14,51 @@ function showSlides() {
 }
 
 
-function validateForm() {
+function validateForm(event) {
+  event.preventDefault();
   const name = document.querySelector('#name');
   const email = document.querySelector('#email');
   const message = document.querySelector('#message');
-  if (name.value === '' || email.value === '' || message.value === '') {
-    return false;
+  const error = document.querySelector('.error');
+  let isValid = true;
+
+
+  const namePattern = /^[A-Za-z\s]+$/;  
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]$/; 
+
+  if (!name.value.match(namePattern)) {
+    isValid = false;
+    error.innerText = 'Enter a valid name!';
+    error.style.display = 'block';
   }
 
-  window.alert("Hi " + name.value + "! Thank you for contacting us!");
+  if (!email.value.match(emailPattern)) {
+    isValid = false;
+    error.innerText = 'Enter a valid email!';
+    error.style.display = 'block';
+  }
+
+  if (message.value.length < 10 || message.value.length > 200) {
+    isValid = false;
+    error.innerText = 'Message should be between 10 and 200 characters!';
+    error.style.display = 'block';
+  }
+
+  if(isValid){
+    error.style.display = 'none';
+    window.alert("Hi " + name.value + "! Thank you for contacting us!");
+   
+  }
+  
   name.value = '';
   email.value = '';
   message.value = '';
-
-  return true; 
+   
 }
 
 const form = document.querySelector('#contact-us form'); 
-form.addEventListener('submit', validateForm);
+form.addEventListener('submit', validateForm)
+
+
+
+
